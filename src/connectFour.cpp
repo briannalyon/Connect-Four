15b6df715b1dee43ba0d18be7150e2cf871af595
Brianna Lyon <briannamlyon@gmail.com>
2021-05-04 10:00:40 -0700

    SFML board added to main

--- src/connectFour.cpp
+++ src/connectFour.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SFML/Graphics.hpp>
+#include <cmath>
 #include "player.h"
 #include "board.h"
 using std::cout;
@@ -12,19 +14,31 @@ int main() {
     Board board;
     bool won = false;
     cout << "Welcome to Connect Four!" << endl;
+    sf::RenderWindow* window = board.getWindow();
 
     while (1) {
-        Player playerOne('X', false, &board); // X stands for a color
-        playerOne.outputBoard();
-        playerOne.move();
-        won = playerOne.hasWon('X');
-        if(won) {break;}
 
-        Player playerTwo('B', false, &board); // X stands for a color
-        playerTwo.outputBoard();
-        playerTwo.move();
-        won = playerTwo.hasWon('B');
-        if(won) {break;}
+        while (window->isOpen()) {	// should this be the game loop
+            sf::Event event;
+            while (window->pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window->close();
+            }
+            board.render();
+            window->display();
+        }
+
+            Player playerOne('X', false, &board); // X stands for a color
+            playerOne.outputBoard();
+            playerOne.move();
+            won = playerOne.hasWon('X');
+            if(won) {break;}
+
+            Player playerTwo('B', false, &board); // X stands for a color
+            playerTwo.outputBoard();
+            playerTwo.move();
+            won = playerTwo.hasWon('B');
+            if(won) {break;}
     }
     return 0;
 }
